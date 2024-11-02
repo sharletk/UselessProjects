bbf136dfa8022d0b5e75bf911855ef4b78484e50
Sharlet Kurien <sharletman@gmail.com>
2024-11-02 20:11:32 +0530

    feat: Connect RTC sensor.

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,17 @@
+#include <Wire.h>
+#include <DS3231.h>
+
 int LED_BUILTIN = 2;
 
 #define IR_SENSOR_PIN 36
+
+DS3231 rtc;
 
 void setup() {
   // pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Hello from ESP32!");
+  Wire.begin(); 
 }
 
 void loop() {
@@ -16,6 +22,9 @@ void loop() {
   if (IR_SENSOR_OUT < 50) {
     Serial.println("Object Close");
   }
+
+  DateTime dt = RTClib::now();
+  Serial.println(dt.second());
 
   delay(500);
 }
